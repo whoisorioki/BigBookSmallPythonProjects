abbf8b0bc31a69ad8e7cf99fceb567214c674df0
Adrian Orioki <adrianorioki@gmail.com>
2023-01-06 14:32:12 +0300

    Update birthday.py

--- birthday.py
+++ birthday.py
@@ -3,4 +3,13 @@ import datetime, random
 def getBirthdays(numberOfBirthdays):
     """Returns a list of number random date objects for birthdays."""
     birthdays = []
-    for i in range
+    for i in range(numberOfBirthdays):
+        # The year is unimportant for our simulation, as long as all
+        # birthdays have same year.
+        startOfYear = datetime.date(2001, 1, 1)
+
+        # Get a random day into the year:
+        randomNumberOfDays = datetime.timedelta(random.randint(0, 364))
+        birthday = startOfYear + randomNumberOfDays
+        birthdays.append(birthday)
+    return birthdays
